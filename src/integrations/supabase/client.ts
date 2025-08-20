f5426c87d6be2cad797f635cd54eeebb80d7d006
TheBattleDog <67001167+krish-jb@users.noreply.github.com>
2025-08-20 21:19:51 +0530

    updated vite-env

--- src/integrations/supabase/client.ts
+++ src/integrations/supabase/client.ts
@@ -1,6 +1,6 @@
 import { createClient } from "@supabase/supabase-js";
 
-const SUPABASE_URL = import.meta.env.VITE_SUPABASE_URL;
-const SUPABASE_PUBLISHABLE_KEY = import.meta.env.VITE_SUPABASE_PUBLIC_KEY;
+const SUPABASE_URL = import.meta.env.VITE_SUPERBASE_URL;
+const SUPABASE_PUBLISHABLE_KEY = import.meta.env.VITE_SUPERBASE_PUBLIC_KEY;
 
 export const supabase = createClient(SUPABASE_URL, SUPABASE_PUBLISHABLE_KEY);
--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -2,10 +2,10 @@
 interface ImportMetaEnv {
     readonly VITE_SUPERBASE_URL: string;
     readonly VITE_SUPERBASE_PUBLIC_KEY: string;
-    readonly VITE_WEBSITE_KEY: string;
+    readonly VITE_TEMPLATE_NAME: string;
     readonly VITE_GALLERY_IMAGE_LIMIT: number;
 }
 
-interface ImportMeta {
+interface MetaImport {
     readonly env: ImportMetaEnv;
 }
